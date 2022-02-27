06021d21bee875f6014717074d43dded9357ab56
teli-siddu <teli.siddu.n@gmail.com>
2022-02-27 15:13:44 +0530

    updated package json

--- src/components/common/OverflowMenu.js
+++ src/components/common/OverflowMenu.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classnames from 'classnames';
+import {FaBeer } from 'react-icons/fa'
 
 export default function OverflowMenu({ children, className, visibilityMap }) {
 
   return (
     <div className={className}>
-    <button className="icon-button">
-          More
-    </button>
+    <span className="icon-button">
+          <FaBeer  />
+    </span>
     {React.Children.map(children, (child) => {
           if (!visibilityMap[child.props["targetId"]]) {
             return (
